ba33184fcbe667ca4ce6e3b6be8d6bd84649b0f3
Natsukie <aproditeloveu@gmail.com>
2020-11-17 17:17:36 -0600

    update add animals

--- animal.js
+++ animal.js
@@ -15,7 +15,31 @@ function animalModule(app, dbName) {
             var dbo = db.db(dbName);
             dbo.collection('users').find({id: req.body.provId, sessionId: req.body.provSessionId}).toArray((err, results) => {
                 if (results.length > 0) {
-                        // 
+                    animalDoc = Object.assign(userDoc, {
+                        // needs: special needs for animal
+                        // brand/number: microchip brand/number
+                        providerId: req.body.providerId,
+                        animalId:idGen.generateSessionId(),
+                        name:req.body.name,
+                        brand:req.body.brand,
+                        number:req.body.number,
+                        birth:req.body.birth,
+                        gender:req.body.gender,
+                        species:req.body.species,
+                        breed:req.body.breed,
+                        color:req.body.color,
+                        size:req.body.size,
+                        weight:req.body.weight,
+                        declawed:req.body.declawed,
+                        location:req.body.location,
+                        status:req.body.status,
+                        needs:req.body.needs
+                    });
+                    dbo.collection('animals').insertOne(userDoc, (err,result) => {
+                        if (err) throw err;
+                        res.status(200).send({ success: true });
+                        db.close();
+                    });
                 }
                 else {
                     res.write(JSON.stringify({ok: false, message: "invalid credentials"}));
